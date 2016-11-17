5e73601d713bc01b80e832849235c6bcac0c7c3c
YenChi-Hsu <yenchi@iis.sinica.edu.tw>
2016-11-17 17:49:52 +0800

    For a new system (ubuntu 16.04 )to install theano and run with gpu.

---
+++ Readme.txt
@@ -0,0 +1,8 @@
+For the new system to install 'Theano', and setup the gpu environment.
+
+First, install theano. (sh theano_setup.sh)
+Second, install nvidia driver and cuda. (sh gpu_setup.sh)
+At the last, copy the theanorc.txt content to write in theanorc.
+
+Then test the check1.py. (python check1.py)
+
--- gpu_setup.sh
+++ gpu_setup.sh
@@ -1,26 +1,15 @@
 
 sudo apt-get install nvidia-current
-sudo apt-get install nvidia-cuda-toolkit
+sudo apt-get install nvidia-cuda-toolkit # As of October 31th, 2014, provide cuda 5.5, not the latest cuda 6.5
 
 wget https://developer.nvidia.com/compute/cuda/8.0/prod/local_installers/cuda-repo-ubuntu1604-8-0-local_8.0.44-1_amd64-deb
+
+mv cuda-repo-ubuntu1604-8-0-local_8.0.44-1_amd64-deb cuda-repo-ubuntu1604-8-0-local_8.0.44-1_amd64.deb
+
 sudo dpkg -i cuda-repo-ubuntu1604-8-0-local_8.0.44-1_amd64.deb
+
 sudo apt-get update
 sudo apt-get install cuda
 
 
-sudo apt-get install g++-4.9
 
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 20
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 10
-
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.9 20
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5 10
-
-sudo update-alternatives --install /usr/bin/cc cc /usr/bin/gcc 30
-sudo update-alternatives --set cc /usr/bin/gcc
-
-sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/g++ 30
-sudo update-alternatives --set c++ /usr/bin/g++
-
-
-THEANO_FLAGS=floatX=float32,device=gpu,nvcc.flags=-D_FORCE_INLINES python
